848c95b1f1494c29259488b6d1f52cdbb7481ac6
+ <+>
2022-03-02 09:28:32 -0800

    commit tree 05f7dd9be43a62a9102ee5deb22257c9f4a44844

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject);
+let userName = "Steve";
+let userEmail = "steve@steve.com";
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: `${userName}`,
+    email: `${userEmail}`,
+  }),
+};
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json())
+    .then((data) => card(data))
+    .catch((error) => errorMessage(error));
+}
+
+function card(data) {
+  const nameFromObject = document.createElement("div");
+  nameFromObject.innerHTML = `<h2>${data.name} ${data.id}</h2>`;
+
+  document.body.append(nameFromObject);
+}
+
+function errorMessage(error) {
+  const message = "Unauthorized Access";
+  const h2Message = document.createElement("h2");
+  h2Message.innerHTML = `<h2> ${error} <h2>`;
+
+  document.body.append(h2Message);
+}
+submitData();
